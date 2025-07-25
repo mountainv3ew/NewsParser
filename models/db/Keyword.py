c7ab60d710a3a59c7fd308885a71c9079994ba38
Литвинюк Тимофей <108889278+4eburaWka@users.noreply.github.com>
2025-07-25 17:12:08 +0300

    Update Keyword.py

--- models/db/Keyword.py
+++ models/db/Keyword.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from sqlalchemy import String, Integer, Text
+from sqlalchemy import String, Integer, Text, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -10,5 +10,5 @@ class UserKeywords(Base):
     __tablename__ = 'user_keywords'
 
     id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    user_id: Mapped[int]
+    user_id: Mapped[int] = mapped_column(BigInteger())
     keywords = mapped_column(Text())
